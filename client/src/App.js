d65bb2d028edc07646c01e5c5dbeb69402379b11
Sayak Mohanta <sayakmohanta1998@gmail.com>
2022-05-28 22:47:29 +0530

    Navbar design

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './app.scss';
+import Home from './pages/Home/Home';
 import {
   ChakraProvider,
   Box,
@@ -14,28 +16,31 @@ import { Logo } from './Logo';
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
+    // <ChakraProvider theme={theme}>
+    //   <Box textAlign="center" fontSize="xl">
+    //     <Grid minH="100vh" p={3}>
+    //       <ColorModeSwitcher justifySelf="flex-end" />
+    //       <VStack spacing={8}>
+    //         <Logo h="40vmin" pointerEvents="none" />
+    //         <Text>
+    //           Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+    //         </Text>
+    //         <Link
+    //           color="teal.500"
+    //           href="https://chakra-ui.com"
+    //           fontSize="2xl"
+    //           target="_blank"
+    //           rel="noopener noreferrer"
+    //         >
+    //           Learn Chakra
+    //         </Link>
+    //       </VStack>
+    //     </Grid>
+    //   </Box>
+    // </ChakraProvider>
+    <div>
+      <Home />
+    </div>
   );
 }
 
