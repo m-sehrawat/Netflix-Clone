8bbd96306544473d3bdab1e3475c67f2bb467d1d
Sayak Mohanta <sayakmohanta1998@gmail.com>
2022-06-11 23:12:04 +0530

    Watch Page added

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './app.scss';
 import { Login } from './pages/Auth/Login';
 import Browse from './pages/Browse/Browse';
+import Watch from './pages/Watch/Watch';
 
 function App() {
   return (
     <div>
-      <Browse />
+      {/* <Browse /> */}
       {/* <Login /> */}
+      <Watch />
     </div>
   );
 }
