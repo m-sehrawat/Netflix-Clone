dbd0027b299869399f0ae469bd1c944e360127e5
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2022-06-12 13:13:50 +0530

    Landing page UI 50% done

--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { Login } from "../pages/Auth/Login";
 import Browse from "../pages/Browse/Browse";
+import { Landing } from "../pages/Landing/Landing";
 
 export const Router = () => {
 
@@ -8,6 +9,7 @@ export const Router = () => {
         <>
             <Routes>
                 <Route path="/" element={<Browse />} />
+                <Route path="/landing" element={<Landing />} />
                 <Route path="/login" element={<Login />} />
             </Routes>
         </>
