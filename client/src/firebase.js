9665a8f3034fcfc9b5325e374a6926e4336d3a2c
Sayak Mohanta <sayakmohanta1998@gmail.com>
2022-06-19 00:41:47 +0530

    API

---
+++ client/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyA9ltGR9WnE10Ibo5btl-Vh1InJ72CGsEE',
+  authDomain: 'netflix-clone-react-fd2f0.firebaseapp.com',
+  projectId: 'netflix-clone-react-fd2f0',
+  storageBucket: 'netflix-clone-react-fd2f0.appspot.com',
+  messagingSenderId: '1098716503699',
+  appId: '1:1098716503699:web:54706563867b97370c3766',
+};
