b5abc09dc5f0fecd644fa3efa9aa69233aa8e09d
Sayak Mohanta <sayakmohanta1998@gmail.com>
2022-06-24 23:49:21 +0530

    Cards API

--- client/src/components/Browse_Components/listItem/ListItem.jsx
+++ client/src/components/Browse_Components/listItem/ListItem.jsx
@@ -7,8 +7,11 @@ import {
 import './listItem.scss';
 import { useState } from 'react';
 
-const ListItem = () => {
+const ListItem = props => {
   const [isHovered, setIsHovered] = useState(false);
+
+  console.log(props.data);
+
   const trailer =
     'https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761';
   return (
@@ -22,7 +25,7 @@ const ListItem = () => {
       }}
     >
       <img
-        src="https://images.unsplash.com/photo-1652680882466-e83b0cccab34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1253&q=80"
+        src={`https://image.tmdb.org/t/p/original/${props.data.backdrop_path}`}
         alt=""
       />
       {isHovered && (
@@ -38,7 +41,7 @@ const ListItem = () => {
             <div className="itemInfoTop">
               <span>1h 14m</span>
               <span className="limit">U/A 16+</span>
-              <span>1999</span>
+              <span>{props.data.release_date}</span>
             </div>
             <div className="desc">Lorem Ipsum is simply dummy text</div>
             <div className="genre">Action - Drama</div>
--- client/src/pages/Browse/Browse.jsx
+++ client/src/pages/Browse/Browse.jsx
@@ -3,14 +3,16 @@ import Featured from '../../components/Browse_Components/featured/Featured';
 import List2 from '../../components/Browse_Components/list/List2';
 import Footer from '../../components/Browse_Components/footer/Footer';
 import './browse.scss';
+import { useState } from 'react';
 
 const Browse = () => {
   return (
     <div className="browse">
       <Navbar />
       <Featured type="movie" />
-      <List2 />
-      <List2 />
+      <List2 type="trending" />
+      <List2 type="originals" />
+      <List2 type="topRated" />
       <Footer />
     </div>
   );
