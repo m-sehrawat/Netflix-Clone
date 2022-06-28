5fa19abc1383e32ce9c4f10c9e7dcdeb1dfd4032
Sayak Mohanta <sayakmohanta1998@gmail.com>
2022-06-29 01:02:43 +0530

    Bug Fixed /

--- client/src/pages/Browse/Browse.jsx
+++ client/src/pages/Browse/Browse.jsx
@@ -9,8 +9,8 @@ const Browse = () => {
     <div className="browse">
       <Navbar />
       <Featured type="movie" />
-      <List2 type="trending" />
-      <List2 type="originals" />
+      {/* <List2 type="trending" /> */}
+      {/* <List2 type="originals" /> */}
       <List2 type="topRated" />
       <Footer />
     </div>
