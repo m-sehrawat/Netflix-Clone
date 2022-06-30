06df442718dca83d1adcc1e242158debfc471d4a
Sayak Mohanta <sayakmohanta1998@gmail.com>
2022-06-30 22:59:12 +0530

    Responsive

--- client/src/API/Requests.js
+++ client/src/API/Requests.js
@@ -9,7 +9,8 @@ export const API_KEY = `9e820f10bac6b6a1aa311da64df90958`;
 export const requests = {
   fetchTrending: `/trending/movie/week?api_key=${API_KEY}&language=en-US`,
   fetchOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  // fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchTopRated: `/movie/latest?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&with_genres=35`,
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&with_genres=27`,
